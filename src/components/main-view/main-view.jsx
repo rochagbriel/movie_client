f59330d4d742acc2d9e9f42bd02d8d383bcc39a6
Gabriel Rocha <rochagbriel@gmail.com>
2023-05-22 23:59:08 +0200

    Feat: The user will stay authenticated between page loads

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,8 +6,8 @@ import { LoginView } from '../login-view/login-view';
 export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
     const storedToken = localStorage.getItem('token');
-    const [user, setUser] = useState(null);
-    const [token, setToken] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser : null);
+    const [token, setToken] = useState(storedToken? storedToken : null);
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
