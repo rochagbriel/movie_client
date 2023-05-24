0ee33925dfeffb42c2313842b0844409cf4fd40e
Gabriel Rocha <rochagbriel@gmail.com>
2023-05-24 14:20:34 +0200

    Refactored login-view bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <Card>
+      <Card 
+      onClick={() => onMovieClick(movie)}
+      style={{ cursor: 'pointer' }}
+      >
         <Card.Img variant='top' src={movie.image} />
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Card.Text>{movie.director}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant='link'>
-            Open
-          </Button>
         </Card.Body>
       </Card>
     );
