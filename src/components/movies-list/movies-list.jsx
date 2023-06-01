f46a72622611ab5657f0c6dc62a6c73373c7f89a
Gabriel Rocha <rochagbriel@gmail.com>
2023-06-01 23:55:06 +0200

    Configured ReduxToolkit
    - Created Feature filter and list

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { MovieCard } from "../movie-card/movie-card";
+import { MoviesFilter } from "../movies-filter/movies-filter";
+import { Col, Row } from "react-bootstrap";
+
+export const MoviesList = () => {
+    const movies = useSelector((state) => state.movies.list);
+    const filter = useSelector((state) => state.movies.filter).trim().toLowerCase();
+
+    const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(filter));
+
+    return (
+        <>
+            <Row>
+                <MoviesFilter />
+            </Row>
+            <Row>
+                {movies.length === 0 ? (
+                    <Col>The list is empty</Col>
+                ) : (
+                    filteredMovies.map((movie) => (
+                        <Col className="mb-4" key={movie.id} md={3}>
+                            <MovieCard movie={movie} />
+                        </Col>
+                    ))
+                )}
+            </Row>
+        </>
+    );
+};
